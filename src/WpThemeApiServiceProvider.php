6c699f24b3bb091c428531ca93b4403e455b1664
Yogi Gilang Ramadhan <yogigilang182@gmail.com>
2024-07-06 11:31:17 +0700

    Creating a Service Provider

---
+++ src/WpThemeApiServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace yogigr\WpThemeApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class WpThemeApiServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        
+    }
+}
